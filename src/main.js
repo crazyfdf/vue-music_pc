562f7002ed81845f992e5bad22b1fdcac15c09fe
crazyfdf <343224608@qq.com>
2020-08-22 17:58:41 +0800

    master

--- src/main.js
+++ src/main.js
@@ -14,30 +14,53 @@ new Vue({
 }).$mount('#app')
 // requestAnimationFrame('./assets/css/base.css')
 
-axios({
-  url: "http://123.207.32.32:8000/home/multidata",
-  method: 'get'
-}).then((res) => {
-  console.log(res);
-});
+// axios.defaults.baseURL = "http://123.207.32.32:8000"
+// axios.defaults.timeout = 5
 
-axios({
-  url: 'http://123.207.32.32:8000/home/data',
-  params: {
-    type: 'pop',
-    page: 1
-  }
-}).then(res => {
-  console.log(res);
-})
-axios.all([axios({
-  url: "http://123.207.32.32:8000/home/multidata",
-}), axios({
-  url: 'http://123.207.32.32:8000/home/data',
-  params: {
-    type: 'sell',
-    page: 5
-  }
-})]).then(results => {
+// axios.all([axios({
+//   url: "http://123.207.32.32:8000/home/multidata"
+// }), axios({
+//   url: 'http://123.207.32.32:8000/home/data',
+//   params: {
+//     type: 'sell',
+//     page: 5
+//   }
+// })]).then(axios.spread((res1, res2) => {
+//   console.log(res1);
+//   console.log(res2);
+// }))
 
+// const instance1 = axios.create({
+//   baseURL: 'http://123.207.32.32:8000',
+//   timeout: 5000
+// })
+// instance1({
+//   url: 'http://123.207.32.32:8000/home/data',
+//   params: {
+//     type: 'sell',
+//     page: 5
+//   }
+// }).then(res => {
+//   console.log(res);
+// })
+// instance1({
+//   url: "http://123.207.32.32:8000/home/multidata"
+// }).then(res => {
+//   console.log(res);
+// })
+
+// const instance2 = axios.create({
+
+// })
+
+import {
+  request
+} from './network/request'
+request({
+  url: '/home/multidata',
+
+}, res => {
+  console.log(res);
+}, err => {
+  console.log(err);
 })
---
+++ src/network/request.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export function request(config, success, failure) {
+	const instance = axios.create({
+		baseURL: 'http://123.207.32.32:8000',
+		timeout: 5000
+	})
+	instance(config)
+		.then(res => {
+			// console.log(res);
+			success(res)
+		})
+		.catch(err => {
+			// console.log(err);
+			failure(err)
+		})
+}
